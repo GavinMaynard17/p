1d8d3aa75cbb0ddaf14e223b05bf7f407f5bfac0
Gavin Maynard <gmay8729@gmail.com>
2024-01-16 17:47:57 -0600

    Update Navbar, populated About

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
   const location = useLocation();
   const [underlineStyle, setUnderlineStyle] = useState({});
   const navItemRefs = useRef([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const activeItem = navItemRefs.current.find(item => item && item.getAttribute('href') === location.pathname);
@@ -38,28 +39,63 @@ const Navbar = () => {
     };
   }, [location]);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const checkSize = () => {
+      if (window.innerWidth > 768) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    // Listen to resize event
+    window.addEventListener('resize', checkSize);
+
+    // Cleanup function
+    return () => {
+      window.removeEventListener('resize', checkSize);
+    };
+  }, []);
+
   return (
-    <div>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="logo-link">
-            Gavin Maynard
-          </Link>
+<div>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="logo-link">Gavin Maynard</Link>
+
+        {/* Toggle Button for Small Screens */}
+        <button className="menu-toggle" onClick={toggleMenu}>☰</button>
+
+        {/* Conditional Rendering for Dropdown Menu */}
+        {isMenuOpen && (
+          <div className="dropdown-menu">
+            {['/', '/about', '/projects', '/contact'].map((path) => (
+              <Link key={path} to={path} onClick={() => setIsMenuOpen(false)}>
+                {path === '/' ? 'Home' : path.substring(1).charAt(0).toUpperCase() + path.substring(2) || 'Home'}
+              </Link>
+            ))}
+          </div>
+        )}
+
+        {/* Conditional Rendering for Nav Links */}
+        {!isMenuOpen && (
           <div className="nav-links">
             {['/', '/about', '/projects', '/contact'].map((path, index) => (
               <NavLink
-              key={path}
-              to={path}
-              label={path === '/' ? 'Home' : path.substring(1).charAt(0).toUpperCase() + path.substring(2) || 'Home'}
-              ref={el => navItemRefs.current[index] = el}
-            />
+                key={path}
+                to={path}
+                label={path === '/' ? 'Home' : path.substring(1).charAt(0).toUpperCase() + path.substring(2) || 'Home'}
+                ref={el => navItemRefs.current[index] = el}
+              />
             ))}
             <div className="underline" style={underlineStyle} />
           </div>
-        </div>
-      </nav>
-
-    </div>
+        )}
+      </div>
+    </nav>
+  </div>
   );
 };
 
